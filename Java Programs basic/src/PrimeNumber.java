7efaaa048eb2aaea0cd313d7e8b0c4ae4754d747
DeepmalaD <deepmala2011dubey@gmail.com>
2019-06-24 09:40:48 +0530

    PrimeNumber

--- Java Programs basic/src/PrimeNumber.java
+++ Java Programs basic/src/PrimeNumber.java
@@ -16,31 +16,35 @@ public class PrimeNumber {
 		}
 			
 
-		else if(num>=2) {
-			
-			for (int i=2;i<=num;i++) {
-		    if(num%i== 0)
-		    	
-		    {
-				
-		    	System.out.println(num +": is not a prime number ");
-
-		        break;
-		    }}}
-		else
-			
+		
+		if(num>=2)
 		{
 			
-			System.out.println(num +": is a prime number ");
-
-		}
+			
+			for (int i=2;i<=num;i++)
+				
+			{
+				
+				if(num%i==0)
+				{
+					System.out.println(num+ ": is not a prime number");
+	
+			}
+				break;
+	
 			}
 		
+
+			
+		}
 		
 		
-		
-		
+		else {
+		System.out.println(num+ ": is  a prime number");
+
+		}
 		
 	}
-
+	
+}
 
